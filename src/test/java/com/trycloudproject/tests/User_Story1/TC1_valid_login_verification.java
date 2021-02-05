64d776d025e0f7fd8c8868a1d87224d055d47dc6
Svetlana <45410378+SvetLanaKo@users.noreply.github.com>
2021-02-04 22:33:36 -0600

    Created User Story1 folder and its 2 TC

---
+++ src/test/java/com/trycloudproject/tests/User_Story1/TC1_valid_login_verification.java
@@ -0,0 +1,4 @@
+package com.trycloudproject.tests.User_Story1;
+
+public class TC1_valid_login_verification {
+}
---
+++ src/test/java/com/trycloudproject/tests/User_Story1/TC2_invalid_login_verification.java
@@ -0,0 +1,4 @@
+package com.trycloudproject.tests.User_Story1;
+
+public class TC2_invalid_login_verification {
+}
